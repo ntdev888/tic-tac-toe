935ba9698a893316dd0d2e664c5d3335e9831f78
trls420s <110872148+trls420s@users.noreply.github.com>
2023-05-12 22:06:05 +1200

    new file:   .vscode/settings.json 	deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	modified:   README.md

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
+
+
+function Square({ value, onSquareClick }) {
+  return (
+  <button className="square" onClick={onSquareClick}>{value}</button>
+  )
+}
+ 
+
+
+function Board() {
+  const [ xIsNext, setXIsNext ] = useState(true)
+  const [ squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  return (
+  <>
+  <div className="board-row">
+    <Square value={squares[0]} onSquareClick={() => handleClick(0)} id="1"/>
+    <Square value={squares[1]} onSquareClick={() => handleClick(1)} id="2"/>
+    <Square value={squares[2]} onSquareClick={() => handleClick(2)} id="3"/>
+    </div>
+    <div className="board-row">
+    <Square value={squares[3]} onSquareClick={() => handleClick(3)} id="3"/>
+    <Square value={squares[4]} onSquareClick={() => handleClick(4)} id="4"/>
+    <Square value={squares[5]} onSquareClick={() => handleClick(5)} id="5"/>
+    </div>
+    <div className="board-row">
+    <Square value={squares[6]} onSquareClick={() => handleClick(6)} id="6"/>
+    <Square value={squares[7]} onSquareClick={() => handleClick(7)} id="7"/>
+    <Square value={squares[8]} onSquareClick={() => handleClick(8)} id="8"/>
+    </div>
+    </>
+  )
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+    <div className="tictactoeapp"><Board /></div>
     </div>
+    
+    </>
   );
 }
 
